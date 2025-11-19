4260059a28d0da9c06f689eb83af727762328c72
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-19 02:15:05 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+#1-misol
+s = "Python2025"
+print(s)
+
+toza = s[6:]
+print(toza)
+
+#2-misol
+s = "abrakadabra"
+print(s)
+
+toza = s[5:8]
+print(toza)
+
+#3-misol
+s = "HelloWorldWideWeb"
+print(s)
+
+toza = s[5:10]
+print(toza)
+
+#4-misol
+s = "123abc456def789"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+#5-misol
+s = "ProgrammingisFun"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+toza2 = toza[::3]
+print(toza2)
+
+#6-misol
+s = "DataSience2025"
+print(s)
+
+toza = s[4:10]
+print(toza)
+
+#7-misol
+s = "abcdefghji"
+print(s)
+
+toza = s[2:8:2] + s[7:1:-1]
+print(toza)
+
+#8-misol
+s = "Xursandchilik"
+print(s)
+
+toza = s[3:7]
+print(toza)
+
+#9-misol
+s = "Al_Revolution_2030"
+print(s)
+toza = s[:2]
+print(toza)
+
+toza2 = s[3:13]
+print(toza2)
+
+toza3 = s[14:19]
+print(toza3)
+
+print(toza + toza2 + toza3)
+
+#10-misol
+s = "qwertyuiop"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+toza2 = toza[1::2]
+print(toza2)
